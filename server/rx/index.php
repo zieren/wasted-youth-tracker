926b4557f00c2cedea2e49b406011b2e9fa69e83
Jörg Zieren <github@zieren.de>
2021-01-24 15:06:35 +0100

    Code cosmetics.

--- server/rx/index.php
+++ server/rx/index.php
@@ -26,7 +26,8 @@ function handleRequest() {
   // TODO: Make trigger time configurable.
   if ($minutesLeftToday <= 0) {
     return "logout";
-  } elseif ($minutesLeftToday <= 5) {
+  }
+  if ($minutesLeftToday <= 5) {
     // TODO: The client shouldn't pop up a message repeatedly. Maybe handle that on the client
     // with two buttons for "snooze" and "dismiss"?
     return "message\n" . $minutesLeftToday . " minutes left today";

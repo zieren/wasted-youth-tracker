fad77ce97a5bb800e889ecc6122e904479b63983
Jörg Zieren <github@zieren.de>
2021-02-14 17:48:29 +0100

    Fail test on DB error.

--- server/tests/KFCTest.php
+++ server/tests/KFCTest.php
@@ -3,30 +3,49 @@
 declare(strict_types=1);
 ini_set('assert.exception', '1');
 
-require_once '../common/base.php';
-
-require_once 'TestCase.php';
-
-require_once '../common/common.php';
-
+require_once '../common/base.php'; // For class loader.
+require_once 'TestCase.php'; // Must initialize Logger before...
+require_once '../common/common.php'; // ... Logger is used here.
 require_once 'config_tests.php';
+require_once '../common/db.class.php';
+
+function logDbQueryErrorInTest($params) {
+  KFCTest::$lastDbError = $params;
+  logDbQueryError($params);
+}
 
 final class KFCTest extends TestCase {
 
+  public static $lastDbError = null;
   private $db;
-  private $mockTime = 1000; // epoch seconds
+  private $mockTime; // epoch seconds, reset to 1000 before each test
 
   protected function setUpTestCase(): void {
     Logger::Instance()->setLogLevelThreshold(\Psr\Log\LogLevel::WARNING);
     $this->db = KFC::createForTest(
-        TEST_DB_NAME, TEST_DB_USER, TEST_DB_PASS, function() { return $this->mockTime(); });
+            TEST_DB_NAME, TEST_DB_USER, TEST_DB_PASS, function() {
+          return $this->mockTime();
+        });
     Logger::Instance()->setLogLevelThreshold(\Psr\Log\LogLevel::DEBUG);
+    // Instantiation of KFC above sets a DB error handler. Override it here.
+    DB::$error_handler = 'logDbQueryErrorInTest';
+    // TODO: Consider checking for errors (error_get_last() and DB error) in production code.
+    // Errors often go unnoticed.
   }
 
   protected function setUp(): void {
     Logger::Instance()->setLogLevelThreshold(\Psr\Log\LogLevel::WARNING);
     $this->db->clearAllForTest();
     Logger::Instance()->setLogLevelThreshold(\Psr\Log\LogLevel::DEBUG);
+    $this->mockTime = 1000;
+  }
+
+  protected function tearDown(): void {
+    if (KFCTest::$lastDbError) {
+      $message = "DB error: " . arrayToString(KFCTest::$lastDbError);
+      KFCTest::$lastDbError = null;
+      throw new Exception($message);
+    }
   }
 
   private function mockTime() {
@@ -93,6 +112,12 @@ final class KFCTest extends TestCase {
     $this->db->insertWindowTitles('user_1', ['window 1', 'window 2'], 0);
     $m3 = $this->db->queryTimeSpentByBudgetAndDate('user_1', $fromTime, null);
     $this->assertEquals($m3, ['' => ['1970-01-01' => 30]]);
+
+    // Add 7 seconds for 'window 2'.
+    $this->mockTime += 7;
+    $this->db->insertWindowTitles('user_1', ['window 2'], 0);
+    $m3 = $this->db->queryTimeSpentByBudgetAndDate('user_1', $fromTime, null);
+    $this->assertEquals($m3, ['' => ['1970-01-01' => 37]]);
   }
 
   public function testSetUpBudgets(): void {
@@ -117,6 +142,33 @@ final class KFCTest extends TestCase {
         ['class_id' => $classId1, 'class_name' => 'c1', 'budget_ids' => [$budgetId1]],
         ['class_id' => $classId2, 'class_name' => 'c2', 'budget_ids' => []],
     ]);
+
+    // TODO: Split this up.
+    // Add a second mapping for the same class.
+    /*$this->db->addMapping('user_1', $classId1, $budgetId2);
+
+    $classification = $this->db->classify(['window 0', 'window 1', 'window 2']);
+
+    // TODO: This fails because classes can only be mapped to one budget (via PK restriction).
+    // Do we want to allow mapping them to multiple budgets? I think yes.
+    // Before we do that and fix this test though: Catch DB errors and surface them.
+
+    $this->assertEquals($classification, [
+        ['class_id' => 1, 'class_name' => DEFAULT_CLASS_NAME, 'budget_ids' => []],
+        ['class_id' => $classId1, 'class_name' => 'c1', 'budget_ids' => [$budgetId1, $budgetId2]],
+        ['class_id' => $classId2, 'class_name' => 'c2', 'budget_ids' => []],
+    ]);*/
+  }
+
+  public function testTimeSpentByTitle(): void {
+    $fromTime = $this->newDateTime();
+    $this->db->insertWindowTitles('user_1', ['window 1', 'window 2'], 0);
+    $this->mockTime += 5;
+    $this->db->insertWindowTitles('user_1', ['window 1', 'window 2'], 0);
+    $this->mockTime += 6;
+    $this->db->insertWindowTitles('user_1', ['window 1', 'window 2'], 0);
+    $timeByTitle = $this->db->queryTimeSpentByTitle('user_1', $fromTime);
+    var_dump($timeByTitle);
   }
 
 }

76f97a7638b04c8f0e5be56d9a989f9e7705836b
Jörg Zieren <github@zieren.de>
2021-03-14 16:11:32 +0100

    Remove error handling stanza, it is not working anyway.

--- server/rx/index.php
+++ server/rx/index.php
@@ -2,8 +2,6 @@
 
 require_once '../common/common.php';
 
-error_reporting(0);
-
 // TODO: Make sure warnings and errors are surfaced appropriately. Catch exceptions.
 
 $content = file_get_contents('php://input');

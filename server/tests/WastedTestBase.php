edabaff766056c0bb21fcba6708b868a6736de13
Jvr for school <109031036+Jvr2022@users.noreply.github.com>
2023-06-16 15:40:19 +0200

    Update WastedTestBase.php
    Added proper PHPDoc comments for functio
    
    Added type hints for function parameters and return types for improved type safety.
    
    Replaced null with ?array for the lastDbError property to indicate it can be null.
    
    Modified the advanceTime method to use the modify method of the DateTime object for better time calculation.
    
    Adjusted the exception throwing condition in the tearDown method to check for null explicitly.

--- server/tests/WastedTestBase.php
+++ server/tests/WastedTestBase.php
@@ -10,18 +10,25 @@ require_once '../common/common.php'; // ... Logger is used here.
 require_once 'config_tests.php';
 require_once '../common/db.class.php';
 
-function logDbQueryErrorInTest($params) {
+/**
+ * Logs the database query error in the test and sets it in the WastedTestBase class.
+ *
+ * @param array $params The parameters of the database query error.
+ * @return void
+ */
+function logDbQueryErrorInTest(array $params): void {
   WastedTestBase::$lastDbError = $params;
   logDbQueryError($params);
 }
 
 class WastedTestBase extends TestCase {
 
-  public static $lastDbError = null;
+  public static ?array $lastDbError = null;
 
   /**
-   * This must happen after Wasted instantiation: Wasted also sets a DB error handler, and we need
-   * to override it here.
+   * Sets the custom database error handler for the WastedTestBase class.
+   *
+   * @return void
    */
   protected function setErrorHandler(): void {
     DB::$error_handler = 'logDbQueryErrorInTest';
@@ -32,20 +39,36 @@ class WastedTestBase extends TestCase {
   }
 
   protected function tearDown(): void {
-    if (WastedTestBase::$lastDbError) {
+    if (WastedTestBase::$lastDbError !== null) {
       $message = "DB error: " . dumpArrayToString(WastedTestBase::$lastDbError);
       throw new Exception($message);
     }
   }
 
-  protected static function advanceTime($seconds): void {
-    Wasted::$now->setTimestamp(Wasted::$now->getTimestamp() + $seconds);
+  /**
+   * Advances the time by the specified number of seconds.
+   *
+   * @param int $seconds The number of seconds to advance the time.
+   * @return void
+   */
+  protected static function advanceTime(int $seconds): void {
+    Wasted::$now->modify("+{$seconds} seconds");
   }
 
+  /**
+   * Returns the current date string in the format 'Y-m-d'.
+   *
+   * @return string The current date string.
+   */
   protected static function dateString(): string {
     return Wasted::$now->format('Y-m-d');
   }
 
+  /**
+   * Returns the current date and time string in the format 'Y-m-d H:i:s'.
+   *
+   * @return string The current date and time string.
+   */
   protected static function dateTimeString(): string {
     return Wasted::$now->format('Y-m-d H:i:s');
   }

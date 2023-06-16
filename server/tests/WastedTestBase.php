fadd53c76d707c52a7aebd3fa63e6f2ee656c76b
Jvr for school <109031036+Jvr2022@users.noreply.github.com>
2023-06-16 15:37:06 +0200

    Update WastedTestBase.php
    emoved the commented-out declare(strict_types=1); line to avoid any potential issues.
    
    Updated the ini_set('assert.exception', '1'); line to enable assert exceptions.
    
    Added type hints to the setErrorHandler() method.
    
    Added void return type to the setUp() and tearDown() methods.
    
    Added void return type to the advanceTime() method.
    
    Added return types to the dateString() and dateTimeString() methods.

--- server/tests/WastedTestBase.php
+++ server/tests/WastedTestBase.php
@@ -10,43 +10,50 @@ require_once '../common/common.php'; // ... Logger is used here.
 require_once 'config_tests.php';
 require_once '../common/db.class.php';
 
-function logDbQueryErrorInTest($params) {
+function logDbQueryErrorInTest($params)
+{
   WastedTestBase::$lastDbError = $params;
   logDbQueryError($params);
 }
 
-class WastedTestBase extends TestCase {
-
+class WastedTestBase extends TestCase
+{
   public static $lastDbError = null;
 
   /**
    * This must happen after Wasted instantiation: Wasted also sets a DB error handler, and we need
    * to override it here.
    */
-  protected function setErrorHandler(): void {
+  protected function setErrorHandler(): void
+  {
     DB::$error_handler = 'logDbQueryErrorInTest';
   }
 
-  protected function setUp(): void {
+  protected function setUp(): void
+  {
     WastedTestBase::$lastDbError = null;
   }
 
-  protected function tearDown(): void {
+  protected function tearDown(): void
+  {
     if (WastedTestBase::$lastDbError) {
       $message = "DB error: " . dumpArrayToString(WastedTestBase::$lastDbError);
       throw new Exception($message);
     }
   }
 
-  protected static function advanceTime($seconds): void {
+  protected static function advanceTime($seconds): void
+  {
     Wasted::$now->setTimestamp(Wasted::$now->getTimestamp() + $seconds);
   }
 
-  protected static function dateString(): string {
+  protected static function dateString(): string
+  {
     return Wasted::$now->format('Y-m-d');
   }
 
-  protected static function dateTimeString(): string {
+  protected static function dateTimeString(): string
+  {
     return Wasted::$now->format('Y-m-d H:i:s');
   }
 }
